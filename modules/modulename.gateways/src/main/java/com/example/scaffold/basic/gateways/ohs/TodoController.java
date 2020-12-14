0218e27898e688f1345a25029a03ed61a621c71f
朱万友 <van@highsoft.ltd>
2020-12-14 13:06:28 +0800

    Rename submodules.

--- modules/modulename.gateways/src/main/java/com/example/scaffold/basic/gateways/ohs/TodoController.java
+++
@@ -1,9 +0,0 @@
-package com.example.scaffold.basic.gateways.ohs;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("todo")
-public class TodoController {
-}
