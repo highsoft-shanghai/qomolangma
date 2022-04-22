c56a160ce8c3bcde13ca6a2e5330da12945f44b9
Neil Wang <wangweili457@gmail.com>
2022-04-22 23:30:15 +0800

    REFACTORED: lines.

--- frameworks/domain.core/src/main/java/com/example/frameworks/domain/core/DomainException.java
+++ frameworks/domain.core/src/main/java/com/example/frameworks/domain/core/DomainException.java
@@ -1,7 +1,6 @@
 package com.example.frameworks.domain.core;
 
 public class DomainException extends RuntimeException {
-
     public DomainException(String message) {
         this(message, null);
     }
