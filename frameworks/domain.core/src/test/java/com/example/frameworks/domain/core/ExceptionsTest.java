0f6386ed61cc01671fc19d7b41cf05d9021c884d
Neil Wang <wangweili457@gmail.com>
2022-04-29 15:24:55 +0800

    DONE: Update ExceptionsTest.

--- frameworks/domain.core/src/test/java/com/example/frameworks/domain/core/ExceptionsTest.java
+++ frameworks/domain.core/src/test/java/com/example/frameworks/domain/core/ExceptionsTest.java
@@ -1,58 +1,54 @@
 package com.example.frameworks.domain.core;
 
+import com.example.frameworks.core.test.WithMocks;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
 import java.io.IOException;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
+@WithMocks
 public class ExceptionsTest {
+    private @Mock TestForExceptions mock;
+
     @Test
     void should_wrap_exception_for_void_functions() throws IOException {
-        TestForExceptions mock = mock(TestForExceptions.class);
         RuntimeException runtimeException = new RuntimeException();
         doThrow(runtimeException).when(mock).execute();
-        Throwable exception = catchThrowable(() -> Exceptions.execute(mock::execute));
-        assertThat(exception).isInstanceOf(RuntimeException.class).isSameAs(runtimeException);
+        assertThatThrownBy(() -> Exceptions.execute(mock::execute)).isInstanceOf(RuntimeException.class).isSameAs(runtimeException);
     }
 
     @Test
     void should_wrap_unchecked_exception_for_void_functions() throws IOException {
-        TestForExceptions mock = mock(TestForExceptions.class);
         doThrow(new IOException()).when(mock).execute();
-        Throwable exception = catchThrowable(() -> Exceptions.execute(mock::execute));
-        assertThat(exception).isInstanceOf(RuntimeException.class);
+        assertThatThrownBy(() -> Exceptions.execute(mock::execute)).isInstanceOf(RuntimeException.class);
     }
 
     @Test
     void should_wrap_exception_for_functions_with_result() throws IOException {
-        TestForExceptions mock = mock(TestForExceptions.class);
         RuntimeException runtimeException = new RuntimeException();
         when(mock.evaluate()).thenThrow(runtimeException);
-        Throwable exception = catchThrowable(() -> Exceptions.evaluate(mock::evaluate));
-        assertThat(exception).isInstanceOf(RuntimeException.class).isSameAs(runtimeException);
+        assertThatThrownBy(() -> Exceptions.evaluate(mock::evaluate)).isInstanceOf(RuntimeException.class).isSameAs(runtimeException);
     }
 
     @Test
     void should_wrap_unchecked_exception_for_functions_with_result() throws IOException {
-        TestForExceptions mock = mock(TestForExceptions.class);
         when(mock.evaluate()).thenThrow(new IOException());
-        Throwable exception = catchThrowable(() -> Exceptions.evaluate(mock::evaluate));
-        assertThat(exception).isInstanceOf(RuntimeException.class);
+        assertThatThrownBy(() -> Exceptions.evaluate(mock::evaluate)).isInstanceOf(RuntimeException.class);
     }
 
     @Test
     void should_be_able_to_execute_method_when_method_is_good() throws IOException {
-        TestForExceptions mock = mock(TestForExceptions.class);
+        when(mock.evaluate()).thenReturn("");
         Exceptions.execute(mock::execute);
         verify(mock, times(1)).execute();
     }
 
     @Test
     void should_be_able_to_evaluate_method_when_method_is_good() throws IOException {
-        TestForExceptions mock = mock(TestForExceptions.class);
         when(mock.evaluate()).thenReturn("Test.");
         String evaluate = Exceptions.evaluate(mock::evaluate);
         verify(mock, times(1)).evaluate();
