31df9678294f4fa6052e1436ac4b0cbb1fd3e3f1
Neil Wang <wangweili457@gmail.com>
2022-04-29 17:06:38 +0800

    DONE: Remove db dependencies.

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM openjdk:11-jdk
+RUN addgroup spring && adduser --ingroup spring spring
+USER spring:spring
+ARG DEPENDENCIES=build/dependencies
+COPY ${DEPENDENCIES}/BOOT-INF/lib /app/lib
+COPY ${DEPENDENCIES}/META-INF /app/META-INF
+COPY ${DEPENDENCIES}/BOOT-INF/classes /app
+ENTRYPOINT ["java", "-cp", "app:app/lib/*", "com.example.scaffold.Application"]
--- src/test/java/com/example/scaffold/TestContainersInitializer.java
+++ src/test/java/com/example/scaffold/TestContainersInitializer.java
@@ -1,18 +1,15 @@
 package com.example.scaffold;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.extension.*;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
 import org.springframework.stereotype.Component;
-import org.testcontainers.containers.MariaDBContainer;
 
 import java.util.stream.Stream;
 
 @Slf4j
 @Component
 public class TestContainersInitializer implements BeforeAllCallback {
-
-    private static MariaDBContainer<?> mysqldb;
-
     @Override
     public void beforeAll(ExtensionContext context) {
         log.info("Starting test containers...");
@@ -20,14 +17,9 @@ public class TestContainersInitializer implements BeforeAllCallback {
     }
 
     private void startMySQL() {
-        if (mysqldb != null) return;
-        mysqldb = new MariaDBContainer<>("mariadb:10.5.8");
-        mysqldb.start();
-        System.setProperty("DB_URI", mysqldb.getJdbcUrl());
     }
 
     public boolean isRunning() {
-        return mysqldb.isRunning();
+        return true;
     }
-
 }
