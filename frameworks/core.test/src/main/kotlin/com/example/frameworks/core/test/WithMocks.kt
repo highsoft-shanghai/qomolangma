23942a0bf06a76790e6cafba3c2cf587677333bb
Neil Wang <wangweili457@gmail.com>
2022-04-30 02:36:13 +0800

    DONE: Rewrite WithMocks by Kotlin.

--- frameworks/core.test/src/main/kotlin/com/example/frameworks/core/test/WithMocks.kt
+++ frameworks/core.test/src/main/kotlin/com/example/frameworks/core/test/WithMocks.kt
@@ -1,18 +1,19 @@
-package com.example.frameworks.core.test;
+package com.example.frameworks.core.test
 
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.junit.jupiter.MockitoSettings;
-import org.mockito.quality.Strictness;
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.junit.jupiter.MockitoSettings
+import org.mockito.quality.Strictness
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
-@Target({ElementType.TYPE, ElementType.METHOD, ElementType.ANNOTATION_TYPE})
-@Retention(RetentionPolicy.RUNTIME)
-@ExtendWith(MockitoExtension.class)
+@Target(
+    AnnotationTarget.ANNOTATION_CLASS,
+    AnnotationTarget.CLASS,
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.PROPERTY_SETTER,
+    AnnotationTarget.ANNOTATION_CLASS
+)
+@Retention(AnnotationRetention.RUNTIME)
+@ExtendWith(MockitoExtension::class)
 @MockitoSettings(strictness = Strictness.LENIENT)
-public @interface WithMocks {
-}
+annotation class WithMocks
