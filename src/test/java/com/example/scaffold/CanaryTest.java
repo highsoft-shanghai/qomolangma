852790c496c4f981d2bf1a6e4d0829596f9ce89d
Neil Wang <wangweili457@gmail.com>
2022-04-30 04:06:16 +0800

    DONE: Rewrite TestContainersInitializer by Kotlin.

--- src/test/java/com/example/scaffold/CanaryTest.java
+++ src/test/java/com/example/scaffold/CanaryTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CanaryTest extends IntegrationTest {
-
     @Test
     void should_be_able_to_run_spring_tests() {
         assertThat("Use-case test environment OK").isNotBlank();
--- src/test/kotlin/com/example/scaffold/TestContainersInitializer.kt
+++ src/test/kotlin/com/example/scaffold/TestContainersInitializer.kt
@@ -1,25 +1,17 @@
-package com.example.scaffold;
+package com.example.scaffold
 
-import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.extension.BeforeAllCallback;
-import org.junit.jupiter.api.extension.ExtensionContext;
-import org.springframework.stereotype.Component;
+import org.junit.jupiter.api.extension.BeforeAllCallback
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.springframework.stereotype.Component
+import java.util.stream.Stream
 
-import java.util.stream.Stream;
-
-@Slf4j
 @Component
-public class TestContainersInitializer implements BeforeAllCallback {
-    @Override
-    public void beforeAll(ExtensionContext context) {
-        log.info("Starting test containers...");
-        Stream.of((Runnable) this::startMySQL).parallel().forEach(Runnable::run);
+class TestContainersInitializer : BeforeAllCallback {
+    override fun beforeAll(context: ExtensionContext) {
+        Stream.of(Runnable { startMySQL() }).parallel().forEach { obj: Runnable -> obj.run() }
     }
 
-    private void startMySQL() {
-    }
-
-    public boolean isRunning() {
-        return true;
-    }
+    private fun startMySQL() {}
+    val isRunning: Boolean
+        get() = true
 }
