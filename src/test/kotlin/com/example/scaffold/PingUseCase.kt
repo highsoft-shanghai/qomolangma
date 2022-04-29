db21b61f639ce8793a2944a6daa52426faeb2f61
Neil Wang <wangweili457@gmail.com>
2022-04-30 04:08:02 +0800

    REFACTORED: PingUseCase.

--- src/test/kotlin/com/example/scaffold/PingUseCase.kt
+++ src/test/kotlin/com/example/scaffold/PingUseCase.kt
@@ -2,11 +2,11 @@ package com.example.scaffold
 
 import com.example.frameworks.application.core.UseCase
 import com.example.frameworks.domain.core.Payload
-import com.example.frameworks.domain.core.StringFieldType
+import com.example.frameworks.domain.core.StringFieldType.Companion.asString
 
 @UseCase
 class PingUseCase {
     fun execute(payload: Payload): String {
-        return payload.get("data", StringFieldType.asString())
+        return payload.get("data", asString())
     }
 }
