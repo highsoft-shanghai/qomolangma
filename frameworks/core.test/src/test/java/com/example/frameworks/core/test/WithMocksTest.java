8f3c255f41c915c7684a0a0f685642aab16e2b41
Neil Wang <wangweili457@gmail.com>
2022-04-29 14:21:41 +0800

    DONE: Add spy tests.

--- frameworks/core.test/src/test/java/com/example/frameworks/core/test/WithMocksTest.java
+++ frameworks/core.test/src/test/java/com/example/frameworks/core/test/WithMocksTest.java
@@ -3,6 +3,7 @@ package com.example.frameworks.core.test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Spy;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
@@ -12,6 +13,7 @@ import static org.mockito.Mockito.verify;
 @WithMocks
 public class WithMocksTest {
     private @Mock WithMocksUnitTest test;
+    private @Spy WithMocksUnitTest test2 = new Impl("test");
 
     @BeforeEach
     void setUp() {
@@ -24,7 +26,26 @@ public class WithMocksTest {
         verify(test, times(1)).evaluate();
     }
 
+    @Test
+    void should_be_able_to_spy_one_object() {
+        assertEquals(test2.evaluate(), "test");
+        verify(test2, times(1)).evaluate();
+    }
+
     interface WithMocksUnitTest {
         String evaluate();
+    }
+
+    static class Impl implements WithMocksUnitTest {
+        private final String word;
+
+        Impl(String word) {
+            this.word = word;
+        }
+
+        @Override
+        public String evaluate() {
+            return word;
+        }
     }
 }
