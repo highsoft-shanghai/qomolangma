71a1245ca90f319cbb8f8967c90eb16a5bdddd5f
Neil Wang <wangweili457@gmail.com>
2022-05-14 21:32:25 +0800

    DONE: Remove useless.

--- frameworks/domain.core/src/test/java/com/example/frameworks/domain/core/JavaAbility.java
+++
@@ -1,7 +0,0 @@
-package com.example.frameworks.domain.core;
-
-public record JavaAbility() {
-    public String run() {
-        return "Hello world.";
-    }
-}
--- frameworks/domain.core/src/test/kotlin/com/example/framework/domain/core/KotlinAbility.kt
+++
@@ -1,7 +0,0 @@
-package com.example.framework.domain.core
-
-class KotlinAbility {
-    fun run(): String {
-        return "Hello world.";
-    }
-}
