5e9fc8eb5668d1098db13e63e253f4b3afd3ed19
Neil Wang <wangweili457@gmail.com>
2022-05-15 16:32:10 +0800

    DONE: Add test for IntegrationTest.

---
+++ frameworks/test.web/src/test/java/com/example/scaffold/frameworks/test/web/TestApplication.java
@@ -0,0 +1,7 @@
+package com.example.scaffold.frameworks.test.web;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+}
