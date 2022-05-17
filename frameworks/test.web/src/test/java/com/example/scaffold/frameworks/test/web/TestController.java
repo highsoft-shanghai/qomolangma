c503e367c3f22fb35e4369ec8b12f5356d46069c
Neil Wang <wangweili457@gmail.com>
2022-05-17 21:29:28 +0800

    DONE: ResponseMatcher.

--- frameworks/test.web/src/test/java/com/example/scaffold/frameworks/test/web/TestController.java
+++ frameworks/test.web/src/test/java/com/example/scaffold/frameworks/test/web/TestController.java
@@ -1,7 +1,9 @@
 package com.example.scaffold.frameworks.test.web;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -13,4 +15,20 @@ public class TestController {
     public Map<String, String> get() {
         return Map.of("id", "123");
     }
+
+    @GetMapping("/bad")
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public void bad() {
+    }
+
+    @GetMapping("/error")
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public void error() {
+    }
+
+    @GetMapping("/created")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void created() {
+    }
+
 }
--- frameworks/test.web/src/test/java/com/example/scaffold/frameworks/test/web/TestResponseTest.java
+++ frameworks/test.web/src/test/java/com/example/scaffold/frameworks/test/web/TestResponseTest.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.Test;
 import javax.annotation.Resource;
 
 import static com.example.scaffold.frameworks.test.web.EqBodyMatcher.eq;
-import static com.example.scaffold.frameworks.test.web.ResponseMatcher.body;
-import static com.example.scaffold.frameworks.test.web.ResponseMatcher.ok;
+import static com.example.scaffold.frameworks.test.web.ResponseMatcher.*;
 
 @IntegrationTest
 public class TestResponseTest {
@@ -15,6 +14,24 @@ public class TestResponseTest {
     @Test
     void should_be_able_to_validate_ok_and_body_eq_when_giving_right_json_rest_api_request() {
         TestResponse response = restTemplate.get("/test");
-        response.is(ok()).is(body("$.id", eq("123")));
+        response.is(ok()).is(body("$.id", eq("123"))).is(textBody("{\"id\":\"123\"}"));
+    }
+
+    @Test
+    void should_be_able_to_validate_bad_request_when_giving_bad_request() {
+        TestResponse response = restTemplate.get("/test/bad");
+        response.is(bad());
+    }
+
+    @Test
+    void should_be_able_to_validate_bad_request_when_giving_error_request() {
+        TestResponse response = restTemplate.get("/test/error");
+        response.is(error());
+    }
+
+    @Test
+    void should_be_able_to_validate_bad_request_when_giving_created() {
+        TestResponse response = restTemplate.get("/test/created");
+        response.is(created());
     }
 }
