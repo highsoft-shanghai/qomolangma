22d9148351864d72e4723a279ae5fa832535eb17
Neil Wang <wangweili457@gmail.com>
2022-05-18 19:11:21 +0800

    REFACTORED: Codes

--- frameworks/test.web/src/main/java/com/example/scaffold/frameworks/test/web/BodyCastStringMatcher.java
+++ frameworks/test.web/src/main/java/com/example/scaffold/frameworks/test/web/BodyCastStringMatcher.java
@@ -1,13 +1,10 @@
 package com.example.scaffold.frameworks.test.web;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-public abstract class BodyCastStringMatcher implements BodyCastTypeMatcher {
-    @Override
-    public void match(Object value) {
-        assertThat(value).isInstanceOf(String.class);
-        realMatch(value);
-    }
-
+public abstract class BodyCastStringMatcher extends BodyCastTypeMatcher {
     protected abstract void realMatch(Object value);
+
+    @Override
+    protected Class<?> typeClass() {
+        return String.class;
+    }
 }
--- frameworks/test.web/src/main/java/com/example/scaffold/frameworks/test/web/BodyCastTypeMatcher.java
+++ frameworks/test.web/src/main/java/com/example/scaffold/frameworks/test/web/BodyCastTypeMatcher.java
@@ -1,4 +1,15 @@
 package com.example.scaffold.frameworks.test.web;
 
-public interface BodyCastTypeMatcher extends ResponseBodyMatcher {
+import static org.assertj.core.api.Assertions.assertThat;
+
+public abstract class BodyCastTypeMatcher implements ResponseBodyMatcher {
+    @Override
+    public void match(Object value) {
+        assertThat(value).isInstanceOf(typeClass());
+        realMatch(value);
+    }
+
+    protected abstract Class<?> typeClass();
+
+    protected abstract void realMatch(Object value);
 }
