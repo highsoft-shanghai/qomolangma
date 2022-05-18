7b6d150b7f05d9114e3eececfd2de36e0417f6cc
Neil Wang <wangweili457@gmail.com>
2022-05-18 13:27:46 +0800

    DONE: Document.

--- frameworks/test.web/src/main/java/com/example/scaffold/frameworks/test/web/ResponseBodyMultiValuesMatcher.java
+++ frameworks/test.web/src/main/java/com/example/scaffold/frameworks/test/web/ResponseBodyMultiValuesMatcher.java
@@ -1,6 +1,20 @@
 package com.example.scaffold.frameworks.test.web;
 
 public interface ResponseBodyMultiValuesMatcher {
+    /**
+     * Verifies that the number of values in the actual group is equal to the given one.
+     * <p>
+     * This assertion will succeed:
+     * <pre><code class='java'> assertThat(new String[] { "a", "b" }).hasSize(2);</code></pre>
+     * <pre><code class='java'> assertThat(Arrays.asList(1, 2, 3)).hasSize(3);</code></pre>
+     * <p>
+     * Whereas this assertion will fail:
+     * <pre><code class='java'> assertThat(new ArrayList()).hasSize(1);</code></pre>
+     * <pre><code class='java'> assertThat(new int[] { 1, 2, 3 }).hasSize(2);</code></pre>
+     * @param value the expected length of the actual group.
+     * @return {@code this} assertion object.
+     * @throws AssertionError if the number of values of the actual group is not equal to the given one.
+     */
     static ResponseBodyMultiValuesMatcher size(int value) {
         return new SizeBodyMatcher(value);
     }
