46067182a7d1111c00c928fc154a010560c438f6
Neil Wang <wangweili457@gmail.com>
2022-06-12 17:38:49 +0800

    DONE: Remove body

--- frameworks/payload.core/src/main/java/com/qomolangma/frameworks/payload/core/ResponsePayload.java
+++ frameworks/payload.core/src/main/java/com/qomolangma/frameworks/payload/core/ResponsePayload.java
@@ -1,8 +1,11 @@
 package com.qomolangma.frameworks.payload.core;
 
+import lombok.Getter;
+
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+@Getter
 public final class ResponsePayload {
     private final Map<String, Object> value;
 
@@ -17,9 +20,5 @@ public final class ResponsePayload {
     public ResponsePayload append(String field, Object value) {
         this.value.put(field, value);
         return this;
-    }
-
-    public Map<String, Object> getValue() {
-        return Map.of("body", this.value);
     }
 }
