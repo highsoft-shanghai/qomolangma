1f806f3abcc6ca477affb1e2caed7dcc6426be61
Neil Wang <wangweili457@gmail.com>
2022-06-14 19:06:12 +0800

    DONE: Fix.

--- frameworks/test.persistence/src/test/java/com/qomolangma/frameworks/test/persistence/PersistenceTestTest.java
+++ frameworks/test.persistence/src/test/java/com/qomolangma/frameworks/test/persistence/PersistenceTestTest.java
@@ -3,13 +3,19 @@ package com.qomolangma.frameworks.test.persistence;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @WithPersistence
 public class PersistenceTestTest {
     private TestClient testClient;
+    private NotTestClient notTestClient;
 
     @Test
     void should_be_able_to_run_test_with_persistence_ability() {
         assertNotNull(testClient);
+        assertNull(notTestClient);
+    }
+
+    private static class NotTestClient {
     }
 }
