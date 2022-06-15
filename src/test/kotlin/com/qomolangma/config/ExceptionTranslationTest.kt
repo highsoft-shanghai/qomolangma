b2f078978e9f2199d7bfca5a33530396f017bbd7
Neil Wang <wangweili457@gmail.com>
2022-06-15 11:30:10 +0800

    DONE: Fix test.

--- src/test/kotlin/com/qomolangma/config/ExceptionTranslationTest.kt
+++ src/test/kotlin/com/qomolangma/config/ExceptionTranslationTest.kt
@@ -1,12 +1,14 @@
 package com.qomolangma.config
 
 import com.qomolangma.frameworks.test.web.IntegrationTest
-import org.hamcrest.Matchers
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.nullValue
 import org.junit.jupiter.api.Test
 
 class ExceptionTranslationTest : IntegrationTest() {
     @Test
     fun should_be_able_to_translate_exception() {
-        get("/test-not-found").statusCode(Matchers.`is`(404))
+        get("/test-not-found").statusCode(`is`(200)).body("code", `is`("1")).body("msg", `is`("nothing found"))
+            .body("data", nullValue())
     }
 }
