4ad00fb4c3adeab7e1a0688330d893ffd63e947f
Neil Wang <wangweili457@gmail.com>
2022-06-28 13:20:48 +0800

    DONE: Fix bug.

--- frameworks/test.web/src/main/kotlin/com/qomolangma/frameworks/test/web/Documentation.kt
+++ frameworks/test.web/src/main/kotlin/com/qomolangma/frameworks/test/web/Documentation.kt
@@ -3,17 +3,17 @@ package com.qomolangma.frameworks.test.web
 import org.springframework.restdocs.snippet.Snippet
 
 class Documentation(private val identifier: String, vararg snippets: Snippet) {
-    private val snippets: Array<Snippet>
+    private val snippets: Array<out Snippet>
 
     init {
-        this.snippets = snippets as Array<Snippet>
+        this.snippets = snippets
     }
 
     fun identifier(): String {
         return identifier
     }
 
-    fun snippets(): Array<Snippet> {
+    fun snippets(): Array<out Snippet> {
         return snippets
     }
 
