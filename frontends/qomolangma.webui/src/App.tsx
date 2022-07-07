79e7024fa23f724b86502777ccec0e16559100d1
Neil Wang <wangweili457@gmail.com>
2022-07-07 11:32:02 +0800

    DONE: Frontend app.

--- frontends/qomolangma.webui/src/App.tsx
+++ frontends/qomolangma.webui/src/App.tsx
@@ -8,6 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Qomolangma frontend app.
+        </p>
+        <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
--- frontends/qomolangma.webui/src/test/App.test.tsx
+++ frontends/qomolangma.webui/src/test/App.test.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import {render, screen} from '@testing-library/react';
 import App from '../App';
 
-test('should renders learn react link', () => {
+test('should renders App', () => {
     render(<App/>)
     const linkElement = screen.getByText(/learn react/i)
     expect(linkElement).toBeInTheDocument()
+    const linkElement2 = screen.getByText(/Qomolangma frontend app./i)
+    expect(linkElement2).toBeInTheDocument()
 });
