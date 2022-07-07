270c4d79a1f30bedc348dc86a247cbd470e99f84
Neil Wang <wangweili457@gmail.com>
2022-07-07 18:52:24 +0800

    DONE: MultiLanguage.tsx

--- frontends/qomolangma.webui/src/index.tsx
+++ frontends/qomolangma.webui/src/index.tsx
@@ -3,17 +3,12 @@ import './index.css';
 import App from './App';
 import {createRoot} from "react-dom/client";
 import {BrowserRouter} from "react-router-dom";
-import {messages} from "./locales/messages";
-import {IntlProvider} from "react-intl";
+import MultiLanguage from "./components/MultiLanguage";
 
 const root = createRoot(document.getElementById("root")!);
-let lang = navigator.language
-if (lang !== 'en-US' && lang !== 'zh-CN') lang = 'en-US'
 
 root.render(
     <BrowserRouter>
-        <IntlProvider locale={lang} messages={messages[lang]}>
-            <App/>
-        </IntlProvider>
+        <MultiLanguage ui={<App/>}/>
     </BrowserRouter>
 );
