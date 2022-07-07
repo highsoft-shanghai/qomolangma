1d0b2effc35b852c234e50f836f10a4da3785b2b
Neil Wang <wangweili457@gmail.com>
2022-07-07 18:24:12 +0800

    DONE: IntlProvider.

--- frontends/qomolangma.webui/src/index.tsx
+++ frontends/qomolangma.webui/src/index.tsx
@@ -3,10 +3,17 @@ import './index.css';
 import App from './App';
 import {createRoot} from "react-dom/client";
 import {BrowserRouter} from "react-router-dom";
+import {messages} from "./locales/messages";
+import {IntlProvider} from "react-intl";
 
 const root = createRoot(document.getElementById("root")!);
+let lang = navigator.language
+if (lang !== 'en-US' && lang !== 'zh-CN') lang = 'en-US'
+
 root.render(
     <BrowserRouter>
-        <App/>
+        <IntlProvider locale={lang} messages={messages[lang]}>
+            <App/>
+        </IntlProvider>
     </BrowserRouter>
 );
