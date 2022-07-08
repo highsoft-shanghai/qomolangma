7404309f4525fce51eac2718a4853395345a85c4
Neil Wang <wangweili457@gmail.com>
2022-07-08 12:24:04 +0800

    DONE: ;

--- frontends/qomolangma.webui/src/test/supports/Http.test.tsx
+++ frontends/qomolangma.webui/src/test/supports/Http.test.tsx
@@ -12,12 +12,12 @@ beforeEach(() => {
 test('should invoke http request', async () => {
     const res = await Http.get('/testHttp/ok');
     expect(res.message).toBe('ok')
-});
+})
 
 test('should invoke get request with params', async () => {
     const res = await Http.get('/testHttp/okWithParam?param=1');
     expect(res.message).toBe('ok')
-});
+})
 
 test('should invoke request report error', async () => {
     try {
@@ -28,7 +28,7 @@ test('should invoke request report error', async () => {
         // eslint-disable-next-line jest/no-conditional-expect
         expect(actual).toBe('Request failed with status code 500')
     }
-});
+})
 
 afterEach(() => {
     scope.removeAllListeners()
