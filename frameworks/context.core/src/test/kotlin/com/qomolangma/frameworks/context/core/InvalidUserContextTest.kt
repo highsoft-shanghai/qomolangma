fdb4e661a51b39c741f3413f648bdea86852d7b5
Neil Wang <wangweili457@gmail.com>
2022-07-08 17:35:03 +0800

    REFACTORED: Static import

--- frameworks/context.core/src/test/kotlin/com/qomolangma/frameworks/context/core/InvalidUserContextTest.kt
+++ frameworks/context.core/src/test/kotlin/com/qomolangma/frameworks/context/core/InvalidUserContextTest.kt
@@ -1,5 +1,6 @@
 package com.qomolangma.frameworks.context.core
 
+import com.qomolangma.frameworks.context.core.UserContext.Companion.INVALID
 import com.qomolangma.frameworks.domain.core.BadAccessTokenException
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
@@ -8,26 +9,26 @@ import org.junit.jupiter.api.Test
 internal class InvalidUserContextTest {
     @Test
     fun should_answer_no_when_test_for_validity() {
-        assertThat(UserContext.INVALID.valid()).isFalse
+        assertThat(INVALID.valid()).isFalse
     }
 
     @Test
     fun should_report_error_when_try_to_get_user_account() {
-        assertThatThrownBy { UserContext.INVALID.userAccount() }.isInstanceOf(
+        assertThatThrownBy { INVALID.userAccount() }.isInstanceOf(
             BadAccessTokenException::class.java
         ).hasMessage("error.bad-token")
     }
 
     @Test
     fun should_report_error_when_try_to_get_user() {
-        assertThatThrownBy { UserContext.INVALID.user() }.isInstanceOf(
+        assertThatThrownBy { INVALID.user() }.isInstanceOf(
             BadAccessTokenException::class.java
         ).hasMessage("error.bad-token")
     }
 
     @Test
     fun should_report_error_when_try_to_get_tenant() {
-        assertThatThrownBy { UserContext.INVALID.tenant() }.isInstanceOf(
+        assertThatThrownBy { INVALID.tenant() }.isInstanceOf(
             BadAccessTokenException::class.java
         ).hasMessage("error.bad-token")
     }
