ab9a2ad2a5f8b9cecfa696064b8d323b3a3d6076
Neil Wang <wangweili457@gmail.com>
2022-07-08 17:38:50 +0800

    REFACTORED: Cov

--- frameworks/context.core/src/test/kotlin/com/qomolangma/frameworks/context/core/SimpleUserContextTest.kt
+++ frameworks/context.core/src/test/kotlin/com/qomolangma/frameworks/context/core/SimpleUserContextTest.kt
@@ -1,7 +1,9 @@
 package com.qomolangma.frameworks.context.core
 
+import com.qomolangma.frameworks.context.core.UserContext.Companion.ANONYMOUS_IDENTITY
 import com.qomolangma.frameworks.domain.core.Identity
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class SimpleUserContextTest {
@@ -23,6 +25,12 @@ internal class SimpleUserContextTest {
     @Test
     fun should_be_able_to_hold_tenant() {
         assertThat(SimpleUserContext(NEIL_IN_QOMOLANGMA, NEIL, QOMOLANGMA).tenant()).isEqualTo(QOMOLANGMA)
+    }
+
+    @Test
+    internal fun should_get_anonymous_identity() {
+        assertEquals("anonymous", ANONYMOUS_IDENTITY.id())
+        assertEquals("Anonymous", ANONYMOUS_IDENTITY.name())
     }
 
     companion object {
