206ebb168214631b2448a3bbacaa811d64e1ce3c
Neil Wang <wangweili457@gmail.com>
2022-07-08 17:50:52 +0800

    DONE: Add module security.core

---
+++ frameworks/security.core/src/test/kotlin/com/qomolangma/frameworks/security/core/CanaryTest.kt
@@ -0,0 +1,11 @@
+package com.qomolangma.frameworks.security.core
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class CanaryTest {
+    @Test
+    internal fun should_run_test() {
+        assertEquals("ok", "ok")
+    }
+}
