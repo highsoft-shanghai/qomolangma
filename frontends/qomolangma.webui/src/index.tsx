bb1376f8ae2d9aa8868ba0af9a34b882b7446684
Neil Wang <wangweili457@gmail.com>
2022-07-08 14:01:53 +0800

    DONE: Each env.

--- frontends/qomolangma.webui/src/index.tsx
+++ frontends/qomolangma.webui/src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './index.css';
 import App from './App';
-import {createRoot} from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import {BrowserRouter} from "react-router-dom";
 import MultiLanguage from "./components/MultiLanguage";
 
-const root = createRoot(document.getElementById("root")!);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
     <BrowserRouter>
