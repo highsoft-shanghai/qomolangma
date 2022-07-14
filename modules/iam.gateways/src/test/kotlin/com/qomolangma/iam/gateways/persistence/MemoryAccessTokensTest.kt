32e47fbb05bddd98875a7d1bae531e3d82ccfea2
Neil Wang <wangweili457@gmail.com>
2022-07-14 16:03:15 +0800

    REFACTORED: Test

--- modules/iam.gateways/src/test/kotlin/com/qomolangma/iam/gateways/persistence/MemoryAccessTokensTest.kt
+++ modules/iam.gateways/src/test/kotlin/com/qomolangma/iam/gateways/persistence/MemoryAccessTokensTest.kt
@@ -1,12 +1,21 @@
 package com.qomolangma.iam.gateways.persistence
 
+import com.qomolangma.iam.domain.AccessToken
 import org.junit.jupiter.api.Test
-import java.util.Optional
+import java.time.Instant
+import java.util.*
 import kotlin.test.assertEquals
 
 internal class MemoryAccessTokensTest {
     @Test
     internal fun should_get_empty_when_no_access_tokens() {
         assertEquals(Optional.empty(), MemoryAccessTokens()["abc"])
+    }
+
+    @Test
+    internal fun should_empty_if_out_of_date() {
+        val memoryAccessTokens = MemoryAccessTokens()
+        memoryAccessTokens.save(AccessToken("id", "token", Instant.EPOCH))
+        assertEquals(Optional.empty(), memoryAccessTokens["token"])
     }
 }
