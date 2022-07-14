d8176100b269669d8372ffce5494c040452a8dff
Neil Wang <wangweili457@gmail.com>
2022-07-14 10:31:31 +0800

    REFACTORED: codes

--- modules/iam.domain/src/main/kotlin/com/qomolangma/iam/domain/AccessTokenContextProvider.kt
+++ modules/iam.domain/src/main/kotlin/com/qomolangma/iam/domain/AccessTokenContextProvider.kt
@@ -6,6 +6,6 @@ import java.util.*
 
 class AccessTokenContextProvider(private val accessTokenRepository: AccessTokens) : ContextProvider {
     override fun get(id: String): Optional<Context> {
-        return accessTokenRepository.optionalAccessTokenFor(id).map { x: AccessToken -> x }
+        return accessTokenRepository.optionalAccessTokenFor(id).map { x -> x }
     }
 }
