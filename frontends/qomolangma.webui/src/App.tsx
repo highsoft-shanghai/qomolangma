ec0d33cb621080afe08691d2d247e22952e7d165
Neil Wang <wangweili457@gmail.com>
2022-07-14 19:08:40 +0800

    DONE: Add route for login

--- frontends/qomolangma.webui/src/App.tsx
+++ frontends/qomolangma.webui/src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import {Route, Routes} from "react-router-dom";
 import Root from "./pages/root";
+import Login from "./pages/login";
 
 const App = () => {
   return (
     <Routes>
+      <Route path="/user/login" element={<Login/>}/>
       <Route path="/" element={<Root/>}/>
     </Routes>
   );
