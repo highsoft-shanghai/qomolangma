fc13e48f71599401a7f41788c9e13a63163f13de
Neil Wang <wangweili457@gmail.com>
2022-07-28 13:07:58 +0800

    DONE: ApiResult warp error words should load in.

--- frameworks/bean.core/src/main/kotlin/com/qomolangma/frameworks/bean/core/ApiResult.kt
+++ frameworks/bean.core/src/main/kotlin/com/qomolangma/frameworks/bean/core/ApiResult.kt
@@ -10,7 +10,7 @@ open class ApiResult<T> protected constructor(request: HttpRequest, body: T?) {
     val code: String
 
     @get:JsonProperty("msg")
-    val message: String
+    val message: String?
     private val data: T?
 
     init {
@@ -23,16 +23,21 @@ open class ApiResult<T> protected constructor(request: HttpRequest, body: T?) {
         return if (StringUtils.endsWith(request.uri.path, "/error")) "1" else "0"
     }
 
-    private fun resolveMessage(body: Any?): String {
+    private fun resolveMessage(body: Any?): String? {
         if (!isError) return ""
         val errors = body as Map<*, *>?
         val error = errors!!["error"] as String?
-        val message = errors["message"] as String?
+        val message = message(errors)
         return if (StringUtils.equals("No message available", message)) if (StringUtils.equals(
                 "Not Found",
                 error
             )
-        ) "not-found" else "unknown" else message!!
+        ) "not-found" else "unknown" else message
+    }
+
+    private fun message(errors: Map<*, *>): String {
+        if (errors.containsKey("message")) return errors["message"] as String
+        return errors["error"] as String
     }
 
     @get:JsonIgnore
