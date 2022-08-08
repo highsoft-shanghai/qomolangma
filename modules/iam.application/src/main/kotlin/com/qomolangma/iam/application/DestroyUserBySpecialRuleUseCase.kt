4e1ac6c251a17b994c2bf77f34b3b5206e159f55
Neil Wang <wangweili457@gmail.com>
2022-08-08 17:57:29 +0800

    DONE: Destroy user account.

--- modules/iam.application/src/main/kotlin/com/qomolangma/iam/application/DestroyUserBySpecialRuleUseCase.kt
+++ modules/iam.application/src/main/kotlin/com/qomolangma/iam/application/DestroyUserBySpecialRuleUseCase.kt
@@ -2,13 +2,16 @@ package com.qomolangma.iam.application
 
 import com.qomolangma.frameworks.application.core.UseCase
 import com.qomolangma.frameworks.security.core.Authorities
+import com.qomolangma.iam.domain.User
 import com.qomolangma.iam.domain.Users
 
 @UseCase(requiredAuthorities = [Authorities.ADMIN, Authorities.SYSTEM])
 class DestroyUserBySpecialRuleUseCase(
-    private val users: Users
+    private val users: Users,
+    private val accessTokens: User.AccessTokens
 ) {
     fun execute(id: String) {
         users.remove(id)
+        accessTokens.remove(id)
     }
 }
--- src/test/kotlin/com/qomolangma/usecase/iam/DestroyUserAccountTest.kt
+++ src/test/kotlin/com/qomolangma/usecase/iam/DestroyUserAccountTest.kt
@@ -6,13 +6,17 @@ import com.qomolangma.frameworks.test.web.Documentation
 import com.qomolangma.frameworks.test.web.Documentation.Companion.doc
 import com.qomolangma.frameworks.test.web.PathVariables.Companion.variables
 import com.qomolangma.frameworks.test.web.WithGrantedAuthorities
+import com.qomolangma.iam.domain.AccessToken
+import com.qomolangma.iam.domain.TokenGenerator
+import com.qomolangma.iam.domain.User
 import com.qomolangma.iam.domain.Users
 import com.qomolangma.usecase.iam.TestUsers.Companion.common
 import org.hamcrest.Matchers.`is`
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.springframework.restdocs.payload.PayloadDocumentation.*
+import org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath
+import org.springframework.restdocs.payload.PayloadDocumentation.responseFields
 import org.springframework.restdocs.request.RequestDocumentation.parameterWithName
 import org.springframework.restdocs.request.RequestDocumentation.pathParameters
 import java.util.*
@@ -23,9 +27,16 @@ class DestroyUserAccountTest : ApiTest() {
     @Resource
     private val users: Users? = null
 
+    @Resource
+    private val accessTokens: User.AccessTokens? = null
+
+    @Resource
+    private val tokenGenerator: TokenGenerator? = null
+
     @BeforeEach
     internal fun setUp() {
         users!!.add(common())
+        accessTokens!!.add(AccessToken.create(common(), tokenGenerator))
     }
 
     @Test
@@ -35,11 +46,13 @@ class DestroyUserAccountTest : ApiTest() {
             .statusCode(`is`(200))
             .body("code", `is`("0"))
         assertSame(Optional.empty(), users!!["1234"])
+        assertSame(Optional.empty(), accessTokens!!["1234"])
     }
 
     @AfterEach
     internal fun tearDown() {
         users!!.clear()
+        accessTokens!!.clear()
     }
 
     override fun document(): Documentation = doc(
