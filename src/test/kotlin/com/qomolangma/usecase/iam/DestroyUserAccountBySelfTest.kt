5784910c6e75a55f0940731a094df441a1dce993
Neil <wangweili457@gmail.com>
2022-09-13 11:22:40 +0800

    DONE: Delete me.

--- src/test/kotlin/com/qomolangma/usecase/iam/DestroyUserAccountBySelfTest.kt
+++ src/test/kotlin/com/qomolangma/usecase/iam/DestroyUserAccountBySelfTest.kt
@@ -6,15 +6,11 @@ import com.qomolangma.frameworks.test.web.Documentation
 import com.qomolangma.frameworks.test.web.Documentation.Companion.doc
 import com.qomolangma.frameworks.test.web.PathVariables.Companion.variables
 import com.qomolangma.frameworks.test.web.WithGrantedAuthorities
-import com.qomolangma.iam.domain.AccessToken
-import com.qomolangma.iam.domain.TokenGenerator
 import com.qomolangma.iam.domain.User
 import com.qomolangma.iam.domain.Users
 import com.qomolangma.usecase.iam.TestUsers.Companion.tester
 import org.hamcrest.Matchers.`is`
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath
 import org.springframework.restdocs.payload.PayloadDocumentation.responseFields
@@ -31,24 +27,15 @@ class DestroyUserAccountBySelfTest : ApiTest() {
     @Resource
     private val accessTokens: User.AccessTokens? = null
 
-    @Resource
-    private val tokenGenerator: TokenGenerator? = null
-
-    @BeforeEach
-    internal fun setUp() {
-        users!!.add(tester)
-        accessTokens!!.add(AccessToken.create(tester, tokenGenerator))
-    }
-
     @Test
     @WithGrantedAuthorities([Authorities.AUTHENTICATED])
-    @Disabled
     internal fun should_destroy_one_account_by_self() {
+        val user = users!!.getByName(tester.owner().user().name())
         delete("/user/me", variables(mapOf<String, String>()), document())
             .statusCode(`is`(200))
             .body("code", `is`("0"))
-        assertSame(Optional.empty(), users!!.getByName(tester.owner().user().name()))
-        assertSame(Optional.empty(), accessTokens!!.getById(""))
+        assertSame(Optional.empty(), users.getByName(tester.owner().user().name()))
+        assertSame(Optional.empty(), accessTokens!!.getById(user.get().id()))
     }
 
     @AfterEach
